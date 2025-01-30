fa5755e0c0a4ba553c330f4ef5798bf492654bad
Zhan-Gabriel <93312041+Zhan-Gabriel-Gerke@users.noreply.github.com>
2025-01-30 11:35:59 +0200

    Update SQL_server_film.sql

--- SQL_server_film.sql
+++ SQL_server_film.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE TAR;
-
-USE TAR;
 CREATE TABLE film(
 filmID int PRIMARY KEY identity(1,1),
 filmNimetus varchar(30) not null,
@@ -39,3 +36,45 @@ FOREIGN KEY(zanrID) REFERENCES zanr(zanrID);
 select *from film;
 select *from zanr;
 UPDATE film SET zanrID=1 where filmID=3
+
+CREATE TABLE rezisoor(
+rezID int PRIMARY KEY identity(1,1),
+nimi varchar(50) not null,
+synniaeg date,
+riik varchar(30)
+);
+
+ALTER TABLE film ADD rezID int;
+SELECT * from rezisoor
+SELECT * from film
+Select * from rezisoor
+SELECT * from kinokava
+ALTER TABLE film ADD CONSTRAINT fk_rezID
+FOREIGN KEY(rezID) REFERENCES rezisoor(rezID);
+
+INSERT INTO rezisoor(nimi, synniaeg, riik)
+VALUES('Zhan Matros', '2001-05-15', 'France')
+
+Select * from rezisoor
+
+CREATE TABLE kinokava(
+kavaID int PRIMARY KEY identity(1,1),
+kuupaevAeg datetime,
+saal varchar(20),
+koht varchar(30)
+);
+
+SELECT * from kinokava
+
+
+ALTER TABLE kinokava ADD filmID int;
+SELECT * from kinokava
+SELECT * from film
+
+ALTER TABLE kinokava ADD CONSTRAINT fk_filmID
+FOREIGN KEY(filmID) REFERENCES film(filmID);
+
+UPDATE film SET zanrID=2 where filmID=5
+
+INSERT INTO kinokava(kuupaevAeg, saal, koht, filmID)
+VALUES('2025-01-30 17:00:00', 'Saal nr 2', 'Coca Cola Plaza', 200);
