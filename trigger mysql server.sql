4f71f22cdb87f7e3069ab0ec66e8cad48d89f670
Zhan-Gabriel <93312041+Zhan-Gabriel-Gerke@users.noreply.github.com>
2025-03-26 09:50:56 +0200

    Update trigger mysql server.sql

--- trigger mysql server.sql
+++ trigger mysql server.sql
@@ -20,7 +20,9 @@ VALUES ('Pärnumaa', 75000);
 ALTER TABLE linnad ADD maakondID int;​
 ALTER TABLE linnad ADD CONSTRAINT fk_maakond​
 FOREIGN KEY (maakondID) References maakond(maakondID)
-
+--drop trigger
+drop trigger linnalisamine
+--
 SELECT * FROM linnad;
 SELECT * FROM maakond;
 INSERT INTO linnad(linnanimi, rahvaarv, maakondID)
@@ -34,6 +36,53 @@ AS
 INSERT INTO logi(aeg, toiming, andmed)
 SELECT GETDATE(),
 'on tehtud INSERT',
-CONCAT(m.maakond, ' linn ',l.linnanimi, ', ', l.rahvaarv)
-FROM linnad l
-INNER JOIN maakond m ON m.maakondID=l.maakondID;
+CONCAT(m.maakond, ' linn ',inserted.linnanimi, ', ', inserted.rahvaarv)
+FROM inserted
+INNER JOIN maakond m ON m.maakondID=inserted.maakondID;
+
+--kontroll
+INSERT INTO linnad(linnanimi, rahvaarv, maakondID)
+VALUES ('Parnu1', 10000, 2)
+SELECT * FROM logi;
+SELECT * FROM linnad;
+
+--delete from logi
+DELETE FROM logi;
+ALTER TABLE logi ADD kasutaja varchar(25);
+CREATE TRIGGER linnaKustutamine
+ON linnad
+FOR DELETE
+AS
+INSERT INTO logi (aeg, toiming, andmed, kasutaja)
+SELECT 
+GETDATE(),
+'on tehtud Delete',
+CONCAT(m.maakond, ' linn ', deleted.linnanimi, ', ', deleted.rahvaarv),
+SUSER_NAME()
+FROM deleted
+INNER JOIN maakond m ON m.maakondID = deleted.maakondID;
+SELECT * FROM linnad;
+
+CREATE TRIGGER linnaUuendamine
+ON linnad
+FOR UPDATE
+AS
+INSERT INTO logi (aeg, toiming, andmed, kasutaja)
+SELECT 
+GETDATE(),
+'on tehtud Update',
+CONCAT('Vana andmed ',m1.maakond, ' linn ', deleted.linnanimi, ', ', deleted.rahvaarv,
+' uued andmed ',m2.maakond, ' linn ', inserted.linnanimi, ', ', inserted.rahvaarv),
+SUSER_NAME()
+FROM deleted
+INNER JOIN inserted ON deleted.linnID=inserted.linnID
+INNER JOIN maakond m1 ON m1.maakondID = deleted.maakondID
+INNER JOIN maakond m2 ON m2.maakondID = deleted.maakondID;;
+SELECT * FROM linnad;
+
+DROP TRIGGER linnaUuendamine;
+--kontroll
+UPDATE linnad SET maakondID = 2
+WHERE linnID=3;
+SELECT * FROM linnad;
+SELECT * FROM logi;
